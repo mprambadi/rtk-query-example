b1dd3b6c7be68396c9089d30e8745a037c1cb2c9
Maulana <mprambadi@gmail.com>
2022-07-06 11:43:49 +0700

    feat: change to json placeholder

--- openapi-config.ts
+++ openapi-config.ts
@@ -1,11 +1,11 @@
 import type { ConfigFile } from '@rtk-query/codegen-openapi'
 
 const config: ConfigFile = {
-  schemaFile: 'https://dn9qobbfc75i5.cloudfront.net/file/data/i2hgz7fy4xbvbg55oob2/PHID-FILE-luqk77ehi32yqi4cjv5z/swagger.yml',
+  schemaFile: 'https://raw.githubusercontent.com/typicode/jsonplaceholder/31e6581ba012d27fd480b052b44001d09e21fdfa/public/swagger.json',
   apiFile: './src/store/emptyApi.ts',
   apiImport: 'emptySplitApi',
-  outputFile: './src/store/userApi.ts',
-  exportName: 'petApi',
+  outputFile: './src/store/jsonPlaceholderApi.ts',
+  exportName: 'jsonPlaceholderApi',
   hooks: true,
   
 }
